270f72746a5a1a0200ad545c90c37ae711e77ac1
Guilherme Ramos <gramos1995@gmail.com>
2017-04-27 22:01:50 +0100

    First iteration of the recognizer

--- src/clos.lisp
+++ src/clos.lisp
@@ -11,6 +11,9 @@
   (intern (concatenate 'string (symbol-name nome-classe) "-" (symbol-name parametro)))
 )
 
+(defun make-symbol-recognizer (nome)
+  (intern (concatenate 'string (symbol-name nome) "?"))
+)
 ;;;
 ;
 ; Def-class helpers
@@ -34,6 +37,10 @@
   )
 )
 
+(defun generate-recognizer (nome atributes)
+  `(defun ,(make-symbol-recognizer nome) (value) (typep value 'vector))
+)
+
 ;;;
 ;
 ; def-class definition
@@ -44,6 +51,7 @@
   `(progn
       ,(generate-constructor nome atributes)
       ,@(generate-getters nome atributes)
+      ,(generate-recognizer nome atributes)
     )
 )
 
