f510d4e7e5bfec8280fe20cbb1332b0f0917e258
Guilherme Ramos <gramos1995@gmail.com>
2017-04-27 10:50:07 +0100

    Basic version of the project

---
+++ src/clos.lisp
@@ -0,0 +1,33 @@
+(defun make-symbol-constructor (nome)
+  (intern (concatenate 'string "MAKE-" (symbol-name nome)))
+)
+
+(defun make-symbol-getters (nome-classe parametro)
+  (intern (concatenate 'string (symbol-name nome-classe) "-" (symbol-name parametro)))
+)
+
+(pprint (macroexpand-1 `(make-symbol-name person)))
+
+(defmacro generate-constructor (nome arguments)
+  `(defun ,(make-symbol-constructor nome) (&key ,@arguments) (vector ,@arguments))
+)
+
+(pprint (macroexpand-1 `(generate-constructor person (nome idade))))
+
+(defmacro def-class (nome &body atributes)
+  `(progn
+      (defun ,(make-symbol-constructor nome) (&key ,@atributes) (vector ,@atributes))
+      ,@(let (
+              (functions '())
+              (parm-i 0)
+             )
+          (loop for param in atributes do
+            (setf functions (append functions (list `(defun ,(make-symbol-getters nome param) (person) (aref person ,parm-i))) ))
+            (setf parm-i (+ parm-i 1))
+          )
+          functions
+       )
+    )
+)
+
+(pprint (macroexpand-1 `(def-class person nome idade)))
