dfee0bcdcf15a2fde8484444564a93c82cec6b5d
Guilherme Ramos <gramos1995@gmail.com>
2017-05-11 10:40:08 +0100

    Fixed recognizers

--- src/clos.lisp
+++ src/clos.lisp
@@ -1,5 +1,31 @@
 (setf classInfo (make-hash-table))
 
+(defun get-class-superclasses (class)
+  (let ((superclassesToExplore (gethash 'superclasses (gethash class classInfo)))
+        (superclasses '()))
+
+    (loop while (not (eq nil superclassesToExplore))
+      do
+      (setf superclasses (append superclasses (list (car superclassesToExplore))))
+      (setf superclassesToExplore (cdr superclassesToExplore))
+    )
+    superclasses
+  )
+)
+
+(defun get-class-subclasses (class)
+  (let ((subclassesToExplore (gethash 'subclasses (gethash class classInfo)))
+        (subclasses '()))
+
+    (loop while (not (eq nil subclassesToExplore))
+      do
+      (setf subclasses (append subclasses (list (car subclassesToExplore))))
+      (setf subclassesToExplore (cdr subclassesToExplore))
+    )
+    subclasses
+  )
+)
+
 ;;;
 ;
 ; Symbol constructors
@@ -25,13 +51,13 @@
 ;;;
 
 (defun generate-constructor (nome arguments)
-  `(defun ,(make-symbol-constructor nome) (&key ,@arguments) (vector ,@arguments))
+  `(defun ,(make-symbol-constructor nome) (&key ,@arguments) (vector ',nome ,@arguments))
 )
 
 (defun generate-getters (nome atributes)
   (let (
         (functions '())
-        (parm-i 0)
+        (parm-i 1)
        )
     (loop for param in atributes do
       (setf functions (append functions (list `(defun ,(make-symbol-getters nome param) (person) (aref person ,parm-i))) ))
@@ -42,22 +68,40 @@
 )
 
 (defun generate-recognizer (nome atributes)
-  `(defun ,(make-symbol-recognizer nome) (value) (and (typep value 'vector) (= ,(length atributes) (length value))))
+  `(defun ,(make-symbol-recognizer nome) (value)
+    (cond ((and (typep value 'vector) (>= (length value) 1) (eq (aref value 0) ',nome)) t)
+          (t (let ((subclassesToCall (mapcar #'(lambda (class) (make-symbol-recognizer class)) (get-class-subclasses ',nome)))
+                   (res nil)
+                  )
+                (loop while (not (eq nil subclassesToCall)) do
+                  (if (funcall (car subclassesToCall) value)
+                    (setf res t)
+                  )
+                  (setf subclassesToCall (cdr subclassesToCall))
+                )
+                res
+             )
+          )
+    )
+   )
 )
 
-(defun registar-class-info (nome atributes offsets)
+(defun registar-class-info (nome atributes offsets superClasses)
   (setf (gethash nome classInfo) (make-hash-table))
   (setf (gethash 'atributes (gethash nome classInfo)) atributes)
   (setf (gethash 'offsets (gethash nome classInfo)) offsets)
-  (setf (gethash nome (gethash 'offsets (gethash nome classInfo))) 0)
+  (setf (gethash nome (gethash 'offsets (gethash nome classInfo))) 1)
+  (setf (gethash 'superclasses (gethash nome classInfo)) superClasses)
+  (setf (gethash 'subclasses (gethash nome classInfo)) '())
+  (mapcar #'(lambda (class) (setf (gethash 'subclasses (gethash class classInfo)) (append (list nome) (gethash 'subclasses (gethash class classInfo)) ))) superClasses)
 )
 
 (defun calculate-offsets (nomeClass classes)
   (let (
-      (offset 0)
+      (offset 1)
      )
     (loop for class in classes do
-      (setf (gethash nomeClass (gethash class classInfo)) offset)
+      (setf (gethash nomeClass (gethash 'offsets (gethash class classInfo))) offset)
       (setf offset (+ (list-length (gethash 'atributes (gethash class classInfo))) offset))
     )
   )
@@ -81,17 +125,19 @@
 (defmacro def-class (nome &body atributes)
   (let ((nomeClass nome)
         (atributesClass atributes)
-        (offsets (make-hash-table)))
+        (offsets (make-hash-table))
+        (superClasses '()))
       (if (listp nome)
           (progn
+            (setf superClasses (cdr nome))
             (setf nomeClass (car nome))
-            (setf atributesClass (calculate-atributes atributes (cdr nome)))
-            (setf (gethash (car (cdr nome)) offsets) 0)
+            (setf atributesClass (calculate-atributes atributes superClasses))
+            (setf (gethash (car superClasses) offsets) 0)
           )
       )
-      (registar-class-info nomeClass atributesClass offsets)
+      (registar-class-info nomeClass atributesClass offsets superClasses)
       (if (listp nome)
-        (calculate-offsets nomeClass (cdr nome))
+        (calculate-offsets nomeClass superClasses)
       )
       `(progn
           ,(generate-constructor nomeClass atributesClass)
---
+++ tests/test02.lisp
@@ -0,0 +1,17 @@
+(def-class person nome idade)
+(def-class worker hours)
+(def-class stuworker coffee redbull)
+(def-class (student person worker stuworker) grades)
+(def-class (studentPro student) sallary)
+
+(setf p (make-person :nome "Teste" :idade 21))
+(setf s (make-student :nome "T" :idade 22 :hours 20 :coffee "black" :redbull 5))
+(setf sp (make-studentPro :nome "T" :idade 22 :hours 20 :coffee "black" :redbull 5 :sallary 10.00))
+
+(person? p) ; t -
+(student? s) ; t -
+(student? p) ; nil
+(person? s) ; t
+(worker? s) ; t
+(stuworker? p) ; nil
+(stuworker? s) ; t
