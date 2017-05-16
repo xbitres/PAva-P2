dfc2ad84d7b22ec3b48e79a116d09322ce37a2d9
Gon19 <goncalos19@gmail.com>
2017-05-16 16:11:09 +0100

    Added comments, and "?" to recognizer function

--- src/clos.lisp
+++ src/clos.lisp
@@ -1,99 +1,23 @@
+;;; Grupo 17 : Guilherme Ramos - 77916 , Goncalo Marques - 78016 , 86318	Aquilino Silva
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; 				Grupo 18 						  	  ;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; 						                         	  ;;;
+;;; HashTable Auxiliar e funcoes auxiliares  			  ;;;
+;;;														  ;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;; HashTable que servira para guardar todas as infromacoes relevantes acerca das classes
 (setf classInfo (make-hash-table))
 
-(defun get-class-superclasses (class)
-  (let ((superclassesToExplore (gethash 'superclasses (gethash class classInfo)))
-        (superclasses '()))
 
-    (loop while (not (eq nil superclassesToExplore))
-      do
-      (setf superclasses (append superclasses (list (car superclassesToExplore))))
-      (setf superclassesToExplore (cdr superclassesToExplore))
-    )
-    superclasses
-  )
-)
-
-(defun get-class-subclasses (class)
-  (let ((subclassesToExplore (gethash 'subclasses (gethash class classInfo)))
-        (subclasses '()))
-
-    (loop while (not (eq nil subclassesToExplore))
-      do
-      (setf subclasses (append subclasses (list (car subclassesToExplore))))
-      (setf subclassesToExplore (cdr subclassesToExplore))
-    )
-    subclasses
-  )
-)
-
-;;;
-;
-; Symbol constructors
-;
-;;;
-
-(defun make-symbol-constructor (nome)
-  (intern (concatenate 'string "MAKE-" (symbol-name nome)))
-)
-
-(defun make-symbol-getters (nome-classe parametro)
-  (intern (concatenate 'string (symbol-name nome-classe) "-" (symbol-name parametro)))
-)
-
-(defun make-symbol-recognizer (nome)
-  (intern (concatenate 'string (symbol-name nome) "?"))
-)
-
-;;;
-;
-; Def-class helpers
-;
-;;;
-
-(defun generate-constructor (nome arguments)
-  `(defun ,(make-symbol-constructor nome) (&key ,@arguments) (vector ',nome ,@arguments))
-)
-
-(defun generate-getters (nome atributes)
-  (let (
-        (functions '())
-        (parm-i 1)
-       )
-    (loop for param in atributes do
-      (setf functions (append functions (list
-        `(defun ,(make-symbol-getters nome param) (class)
-          (let ((offset (gethash (aref class 0) (gethash 'offsets (gethash ',nome classInfo)))))
-            (if (not (eq nil offset))
-              (aref class (+ ,parm-i offset))
-            )
-          )
-         )
-        )))
-      (setf parm-i (+ parm-i 1))
-    )
-    functions
-  )
-)
-
-(defun generate-recognizer (nome atributes)
-  `(defun ,(make-symbol-recognizer nome) (value)
-    (cond ((and (typep value 'vector) (>= (length value) 1) (eq (aref value 0) ',nome)) t)
-          (t (let ((subclassesToCall (mapcar #'(lambda (class) (make-symbol-recognizer class)) (get-class-subclasses ',nome)))
-                   (res nil)
-                  )
-                (loop while (not (eq nil subclassesToCall)) do
-                  (if (funcall (car subclassesToCall) value)
-                    (setf res t)
-                  )
-                  (setf subclassesToCall (cdr subclassesToCall))
-                )
-                res
-             )
-          )
-    )
-   )
-)
-
+;;;	registar-class-info: nome atributos offsets superCLasses
+;;;	Esta funcao regista na tablea o nome, atributos, offsets e superClasses
 (defun registar-class-info (nome atributes offsets superClasses)
   (setf (gethash nome classInfo) (make-hash-table))
   (setf (gethash 'atributes (gethash nome classInfo)) atributes)
@@ -140,12 +64,116 @@
   )
 )
 
-;;;
-;
-; def-class definition
-;
-;;;
+;;;	get-class-superclasses: class -> lista de superclasses
+;;;	Esta funcao recebe uma classe, e atraves da informacao na hashtable,
+;;;	retorna uma lista com todas as superclasses da classe recebida
+(defun get-class-superclasses (class)
+  (let ((superclassesToExplore (gethash 'superclasses (gethash class classInfo)))
+        (superclasses '()))
 
+    (loop while (not (eq nil superclassesToExplore))
+      do
+      (setf superclasses (append superclasses (list (car superclassesToExplore))))
+      (setf superclassesToExplore (cdr superclassesToExplore))
+    )
+    superclasses
+  )
+)
+
+
+;;;	get-class-subclasses: class -> lista de superclasses
+;;;	Esta funcao recebe uma classe, e atraves da informacao na hashtable,
+;;;	retorna uma lista com todas as subclasses da classe recebida
+(defun get-class-subclasses (class)
+  (let ((subclassesToExplore (gethash 'subclasses (gethash class classInfo)))
+        (subclasses '()))
+
+    (loop while (not (eq nil subclassesToExplore))
+      do
+      (setf subclasses (append subclasses (list (car subclassesToExplore))))
+      (setf subclassesToExplore (cdr subclassesToExplore))
+    )
+    subclasses
+  )
+)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; 						                         	  ;;;
+;;;	Symbol Constructors  				                  ;;;
+;;;														  ;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun make-symbol-constructor (nome)
+  (intern (concatenate 'string "MAKE-" (symbol-name nome)))
+)
+
+(defun make-symbol-getters (nome-classe parametro)
+  (intern (concatenate 'string (symbol-name nome-classe) "-" (symbol-name parametro)))
+)
+
+(defun make-symbol-recognizer? (nome)
+  (intern (concatenate 'string (symbol-name nome) "?"))
+)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; 						                         	  ;;;
+;;;	Functions generators  				                  ;;;
+;;;														  ;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun generate-constructor (nome arguments)
+  `(defun ,(make-symbol-constructor nome) (&key ,@arguments) (vector ',nome ,@arguments))
+)
+
+(defun generate-getters (nome atributes)
+  (let (
+        (functions '())
+        (parm-i 1)
+       )
+    (loop for param in atributes do
+      (setf functions (append functions (list
+        `(defun ,(make-symbol-getters nome param) (class)
+          (let ((offset (gethash (aref class 0) (gethash 'offsets (gethash ',nome classInfo)))))
+            (if (not (eq nil offset))
+              (aref class (+ ,parm-i offset))
+            )
+          )
+         )
+        )))
+      (setf parm-i (+ parm-i 1))
+    )
+    functions
+  )
+)
+
+(defun generate-recognizer? (nome atributes)
+  `(defun ,(make-symbol-recognizer? nome) (value)
+    (cond ((and (typep value 'vector) (>= (length value) 1) (eq (aref value 0) ',nome)) t)
+          (t (let ((subclassesToCall (mapcar #'(lambda (class) (make-symbol-recognizer? class)) (get-class-subclasses ',nome)))
+                   (res nil)
+                  )
+                (loop while (not (eq nil subclassesToCall)) do
+                  (if (funcall (car subclassesToCall) value)
+                    (setf res t)
+                  )
+                  (setf subclassesToCall (cdr subclassesToCall))
+                )
+                res
+             )
+          )
+    )
+   )
+)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; 						                         	  ;;;
+;;;	Macro Definition	  				                  ;;;
+;;;														  ;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;	def-class
+;;;	Esta macro
+;;;
 (defmacro def-class (nome &body atributes)
   (let ((nomeClass nome)
         (atributesClass atributes)
@@ -166,7 +194,7 @@
       `(progn
           ,(generate-constructor nomeClass atributesClass)
           ,@(generate-getters nomeClass atributesClass)
-          ,(generate-recognizer nomeClass atributesClass)
+          ,(generate-recognizer? nomeClass atributesClass)
         )
     )
 )
